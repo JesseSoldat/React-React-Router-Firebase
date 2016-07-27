ae8414d7eab449c924c23bd28f9f9b4ae2cb649d
jesse soldat <jsoldat@hotmail.com>
2016-07-27 16:51:13 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from'react-dom';
+
+import {Router, Route, IndexRoute, Link, hashHistory, browserHistory } from 'react-router';
+
+
+import {
+	AppComponent
+} from './views';
+
+ReactDom.render(<AppComponent/>, document.getElementById('root'));
